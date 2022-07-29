0c0da845756b125a73f6657866d2f8db1b045dcc
gomezhenry77 <gomezhenry77@gmail.com>
2022-07-29 16:03:09 -0500

    instrucciones javascript

---
+++ app.js
@@ -0,0 +1,36 @@
+/* let nombre="Henry Gomez"
+let edad=33
+let nombreCliente="Camilo"
+let edadCliente=22
+let profesionCliente="estudiante" */
+/* console.log("mi nombre es: "+nombre+" y tengo "+edad+" años") */
+
+/* let nombre=prompt("ingrese el nombre")
+let edad=prompt("ingrese la edad")
+console.log("mi nombre es: "+nombre+" y tengo "+edad+" años") */
+
+/* interpolación con backtis: */
+/* console.log(`El estudiante ${nombre} tiene ${edad} años`); */
+
+let cantidadHora=5
+let valorHora=2500
+let sueldo=cantidadHora*valorHora
+if (cantidadHora<24){
+    let aumento=sueldo*10/100
+    /* console.log("no puede trabajar menos de 24 horas"); */
+    console.log(`el aumento es de ${aumento}`);
+    let salarioTotal=sueldo+aumento
+    console.log(`el salario total es ${salarioTotal}`);
+}
+
+/* console.log(`el sueldo es ${sueldo}`); */
+
+
+
+
+
+
+
+
+
+
